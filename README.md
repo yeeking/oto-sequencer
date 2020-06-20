5b274cf63fa1a12389883f83c176eb7da9b9bc12
Matthew Yee-King on yogurt <m.yee-king@gold.ac.uk>
2020-06-20 18:23:39 +0100

    reasonable cursor control in console

--- README.md
+++ README.md
@@ -2,4 +2,27 @@
 
 A sound sequencer. Runs on raspberry pi. 
 
+To build, first set up the cmake stuff:
+```
+   mkdir build
+   cd build
+   cmake ..
+```
+
+On raspberry pi (using grove sensors):
+```
+   make oto-sequencer-pi
+   ./oto-sequencer-pi
+```
+
+On normal linux or raspi linux without grove sensors:
+
+```
+  make oto-sequencer
+  ./oto-sequencer
+```
+
+
+
+
 
--- src/Main.cpp
+++ src/Main.cpp
@@ -1,5 +1,7 @@
 #include <functional>
 #include <iostream>
+#include <stdio.h>  
+
 
 //#include "../lib/grovepi.h"
 //#include "../lib/grove_rgb_lcd.h"
@@ -10,6 +12,39 @@
 //#include "GroveUtils.h"
 #include "RapidLibUtils.h"
 
+
+#include <unistd.h>
+#include <termios.h>
+
+// getch with no echo and instant response
+// https://stackoverflow.com/a/912796/1240660
+char getch() {
+        char buf = 0;
+        struct termios old = {0};
+        if (tcgetattr(0, &old) < 0)
+                perror("tcsetattr()");
+        old.c_lflag &= ~ICANON;
+        old.c_lflag &= ~ECHO;
+        old.c_cc[VMIN] = 1;
+        old.c_cc[VTIME] = 0;
+        if (tcsetattr(0, TCSANOW, &old) < 0)
+                perror("tcsetattr ICANON");
+        if (read(0, &buf, 1) < 0)
+                perror ("read()");
+        old.c_lflag |= ICANON;
+        old.c_lflag |= ECHO;
+        if (tcsetattr(0, TCSADRAIN, &old) < 0)
+                perror ("tcsetattr ~ICANON");
+        return (buf);
+}
+
+void redraw(Sequencer& seqr, SequencerEditor& seqEditor)
+{
+  std::cout << "\x1B[2J\x1B[H";
+  std::string disp = SequencerViewer::toTextDisplay(8, 32, &seqr, &seqEditor);
+  std::cout << disp << std::endl;
+}
+
 int main()
 {
     Sequencer seqr;
@@ -19,22 +54,51 @@ int main()
     rapidLib::regression network = NeuralNetwork::getMelodyStepsRegressor();
 
     clock.setCallback([&seqr, &seqEditor](){
-        seqr.tick();
-            // clear the screen in nix
-            std::cout << "\x1B[2J\x1B[H";
-
-            std::string disp = SequencerViewer::toTextDisplay(2, 16, &seqr, &seqEditor);
-            std::cout << disp << std::endl;
+            seqr.tick();
+            redraw(seqr, seqEditor);    
         });
 
-//    clock.start(1000);
-    
 
-    
-    int x {1};
-    while (x != 0)
+   clock.start(1000);
+ 
+
+    char x {1};
+    bool escaped = false;
+    while (x != 'x')
     {
-      std::cin >> x;
-      std::cout << x << std::endl;  
+      x = getch();
+      if (x == '\033') {
+        escaped = true;
+        continue;
+      }
+      if (escaped){
+        switch(x){
+          case '[': 
+            continue;
+          case 'A':
+            // up
+            seqEditor.moveCursorUp();
+            redraw(seqr, seqEditor);
+            continue;
+          case 'D':
+            // left
+            seqEditor.moveCursorLeft();
+            redraw(seqr, seqEditor);
+            continue;
+          case 'C':
+            // right
+            seqEditor.moveCursorRight();
+            redraw(seqr, seqEditor);
+            continue;
+          case 'B':
+            // down
+            seqEditor.moveCursorDown();
+            redraw(seqr, seqEditor);
+            continue;     
+        }
+      }
     }
+
+  
+  return 0;
 }
